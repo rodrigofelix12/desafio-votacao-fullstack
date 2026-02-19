857aaa5bd41fa1a8643fb68f89dbdc714c14f7fb
Rodrigo Felix <rodrigo.felix321@gmail.com>
2026-02-19 16:54:31 -0300

    criacao projeto api

---
+++ desafio-votacao-api/src/main/java/dev/rodrigo/desafiovotacao/DesafioVotacaoApiApplication.java
@@ -0,0 +1,13 @@
+package dev.rodrigo.desafiovotacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVotacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVotacaoApiApplication.class, args);
+	}
+
+}
---
+++ desafio-votacao-api/src/test/java/dev/rodrigo/desafiovotacao/DesafioVotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rodrigo.desafiovotacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
