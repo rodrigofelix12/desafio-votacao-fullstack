fc27b2ccd06f9da67f7d73a2e2f64d3173cfe053
Rodrigo Felix <rodrigo.felix321@gmail.com>
2026-02-22 15:06:33 -0300

    feat: criacao e configuracao front react

---
+++ votacao-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sessao from "./pages/Sessao";
+import Resultado from "./pages/Resultado";
+import { ToastContainer } from "react-toastify";
+
+export default function App() {
+  return (
+    <Router>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sessao/:id" element={<Sessao />} />
+        <Route path="/resultado/:id" element={<Resultado />} />
+      </Routes>
+    </Router>
+  );
+}
